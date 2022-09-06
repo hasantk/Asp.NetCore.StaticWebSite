acf4a3c232d01923dc4c28b7b379d16d0fc2db81
Hasan Atık <hasan.atik16860@gmail.com>
2022-09-06 13:30:34 +0300

    Asp.NetCore.StaticWebSite
    Asp.Net Core6.0 Static WebSite

---
+++ AspNet.Core.Web/AspNet.Core.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNet.Core.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
